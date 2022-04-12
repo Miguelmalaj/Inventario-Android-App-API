0ee3adb2ef69e308bba123e022d98f4a9c286c8f
Miguelmlj <hsmiguelhs@gmail.com>
2022-04-12 12:21:07 -0600

    test with retrofit next

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ export default {
     port: 3000,
     dbUser: "userroot",
     dbPassword: "1234567890",
-    dbServer: "localhost",
+    dbServer: "10.10.10.52",
     dbDatabase: "consolidado_ventas"
 }
