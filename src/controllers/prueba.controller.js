185753ebafb091069534f13993c18cf1208d70ef
Miguelmlj <hsmiguelhs@gmail.com>
2022-04-12 12:49:06 -0600

    aaa

--- src/controllers/prueba.controller.js
+++ src/controllers/prueba.controller.js
@@ -4,7 +4,7 @@ export const getUsuarios = async (req, res) => {
     try {
         const pool = await getConnection();
         const result = await pool.request().query(querys.prueba);
-        res.json(result.recordset);
+        res.json({usuarios:result.recordset});
       } catch (error) {
         res.status(500);
         res.send(error.message);
